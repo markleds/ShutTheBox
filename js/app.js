9e38b835fa530134d58876ca0f88cb3214111086
Mark Ledbetter <markleds@gmail.com>
2017-05-11 21:53:50 -0400

    Addition of animation to dice; addition of image to cover played numbers

--- js/app.js
+++ js/app.js
@@ -20,45 +20,53 @@ $(document).ready(function() {
   // Start of game constant for the sum of selected numbers on Number Line
   var sumSelectedNumbers = 0;
 
+  var $dice1 = $("#dice-1");
+  var $dice2 = $("#dice-2");
+  var $dice = $(".dice");
+
   // Dice background image classes array
   var diceImageClasses = ["dice-1", "dice-2", "dice-3", "dice-4", "dice-5",
     "dice-6"
   ];
-  var $dice1 = $("#dice-1");
-  var $dice2 = $("#dice-2");
+
   // 2 variables to hold the index number for the dye backgroud image class
   var dice1Index;
   var dice2Index;
 
   // variable to hold the sum of the dice for comparison to players selections on Nmber Line - updated on every roll of the dice.
   var diceSum = 0;
-  // variable to keep track of the number of times the user rolls the dice in each game
+
+  // Constant to keep track of the number of times the user rolls the dice in each game
   var diceRolls = 0;
-  // Variable for the fewest rolls of the dice to win the game
+
+  // Constant for the fewest rolls of the dice to win the game
   var recordDiceRolls = 0;
+
   // Number of games played
   var gamesPlayed = 0;
   var gamesWon = 0;
   var $gamesPlayed = $(".games-played");
   var $gamesWon = $(".games-won");
 
+  var time = 0;
+  var $timer = $(".timer");
+  var $recordTime = $(".record-time");
+
   // functions to give you a number between and including 0 & 5 for the index of dice background image classs in the array diceImageClasses
   var dice1Bkgnd;
   var dice2Bkgnd;
   var getDice1Bkgnd = function() {
-    dice1Index = Math.floor(Math.random() * 5);
+    dice1Index = Math.floor(Math.random() * 6);
     dice1Bkgnd = diceImageClasses[dice1Index];
-    // console.log(dice1Bkgnd);
   };
   var getDice2Bkgnd = function() {
-    dice2Index = Math.floor(Math.random() * 5);
+    dice2Index = Math.floor(Math.random() * 6);
     dice2Bkgnd = diceImageClasses[dice2Index];
-    // console.log(dice2Bkgnd);
   };
 
   // Popup Event listeners & functionality
   var $letsPlay = $("#lets-play");
-  var $closePopup = $(".close-popup");
+  var $closePopup = $("#close-popup");
   var $closeWinPopup = $(".close-win-popup");
   var $howToPlay = $("#instructions");
   var $incorrectPlay = $("#incorrect-play");
@@ -68,27 +76,23 @@ $(document).ready(function() {
   var $winPopup = $("#win-popup");
   var $backToGame = $("#back-to-game");
   var $playAgain = $("#play-again");
-  // Event Listener to display Popup
+  // Event Listener to display Instructions Popup
   $howToPlay.on("click", function() {
     $popupCover.fadeIn(1000);
     $howToWindow.fadeIn(1000);
   });
-  // Event Listener to hide popup
-  $letsPlay.on("click", function() {
+  // Event Listener to hide Instructions popup
+  $("#lets-play, #close-popup").on("click", function() {
     $popupCover.fadeOut(1000);
     $howToWindow.fadeOut(1000);
-  });
-  $closePopup.on("click", function() {
-    $popupCover.fadeOut(1000);
-    $howToWindow.fadeOut(1000);
-    $incorrectPlay.fadeOut(1000);
   });
   // function to display a popup when the incorrect numbers are selected
   var incorrectPopup = function() {
     $popupCover.fadeIn(1000);
     $incorrectPlay.fadeIn(1000);
   };
-  $backToGame.on("click", function() {
+  // return to game
+  $("#back-to-game, #close-popup").on("click", function() {
     $popupCover.fadeOut(1000);
     $incorrectPlay.fadeOut(1000);
   });
@@ -97,19 +101,13 @@ $(document).ready(function() {
     $winCover.fadeIn(1000);
     $winPopup.fadeIn(1000);
   };
-  // function to close win popup window and re-set game
-  var closeWinPopup = function() {
+  // Event Listener to close win popup window and re-set game
+  $("#close-win-popup, #play-again").on("click", function() {
     $winCover.fadeOut(1000);
     $winPopup.fadeOut(1000);
     setNumbers();
     numbersPlayed = [];
     sumSelectedNumbers = 0;
-  };
-  $closeWinPopup.on("click", function() {
-    closeWinPopup();
-  });
-  $playAgain.on("click", function() {
-    closeWinPopup();
   });
 
   // Function that checks to see if you have won the game
@@ -121,11 +119,11 @@ $(document).ready(function() {
       gamesPlayed++;
       $gamesPlayed.text(gamesPlayed);
       $numDiv.removeClass("selected played");
+      compareDiceRolls();
     } else {
       return;
     }
   };
-  // console.log($tenPlayed.length);
 
   // function to black out numbers that have already been played successfully
   var playedNumbers = function() {
@@ -144,6 +142,8 @@ $(document).ready(function() {
   };
   var rollTheDice = function() {
 
+    spinDice();
+
     // remove dice background class
     $dice1.removeClass(dice1Bkgnd);
     $dice2.removeClass(dice2Bkgnd);
@@ -159,10 +159,22 @@ $(document).ready(function() {
     $dice1.addClass(dice1Bkgnd);
     $dice2.addClass(dice2Bkgnd);
 
+
     winGame();
     diceRollCount();
-
   };
+  var spinDice = function() {
+    setTimeout(function() {
+      $(".dice").addClass("roll-dice-1");
+    }, 20);
+    setTimeout(function() {
+      $(".dice").removeClass("roll-dice-1").addClass("roll-dice-2");
+    }, 600);
+    setTimeout(function() {
+      $(".dice").removeClass("roll-dice-2");
+    }, 1200);
+  };
+
 
   // Function that toggles the class "selected" on the numbers when clicked on
   $numDiv.on("click", function() {
@@ -201,8 +213,6 @@ $(document).ready(function() {
     for (let i = 0; i < selectedNumbersArray.length; i++) {
       sumSelectedNumbers += parseInt(selectedNumbersArray[i].innerHTML);
     }
-    // console.log(sumSelectedNumbers);
-    // console.log(diceSum);
     if (sumSelectedNumbers === 0) {
       rollTheDice();
     } else if (sumSelectedNumbers !== diceSum) {
@@ -210,7 +220,6 @@ $(document).ready(function() {
       $numDiv.removeClass("selected");
     } else {
       playedNumbers();
-      // winGame();
       rollTheDice();
     }
   };
@@ -220,8 +229,8 @@ $(document).ready(function() {
       rollDiceCompleteTurn();
     }
   });
-  // Event listener on Roll Dice button to roll the dice and check selected numbers
-  $rollDiceButton.on("click", function() {
+  // Event listener on Roll Dice button and individual Dye to roll the dice and check selected numbers
+  $("#roll-dice, .dice").on("click", function() {
     rollDiceCompleteTurn();
   });
 
@@ -241,7 +250,6 @@ $(document).ready(function() {
   // Play again
   var $playAgainButton = $("#play-again");
   $playAgainButton.on("click", function() {
-    compareDiceRolls();
     $numDiv.removeClass("selected played");
     setNumbers();
     diceRolls = 0;
@@ -266,14 +274,15 @@ $(document).ready(function() {
     // update variable diceSum with new sum of rolled dice
     diceSum = (dice1Index + 1) + (dice2Index + 1);
     console.log(diceSum);
-    // console.log(diceSum);
+
     // add new dice background class to update dice background image
     $dice1.addClass(dice1Bkgnd);
     $dice2.addClass(dice2Bkgnd);
     diceRolls = 0;
     diceRollCount();
+
   };
   setNumbers();
-
+  spinDice();
 
 });
